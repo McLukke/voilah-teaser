ea07154e681d00e02eed6e4810015886c364c8ec
Kenny Lu <Ken@Defyent.com>
2015-04-15 19:56:13 +0800

    redoing slides for fade

--- router.js
+++ router.js
@@ -1,29 +1,21 @@
-// Router.map(function () {
-//   this.route('home', {
-//     path: '/',
-//     template: 'slide1',
-//     layoutTemplate: 'layout',
-//     yieldTemplate: {
-//       'openingSlide': {to: 'aside'}
-//     }
-//   });
-// });
-
 Router.configure({
-  layoutTemplate: 'layout'
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading'
 });
 
 Router.route('/', function () {
   this.render('openingSlide', {to: 'opener'});
-  if ( Session.get('currentSlide') === 1 ) {
-    this.render('slide1');
-  } else if ( Session.get('currentSlide') === 2 ) {
-    this.render('slide2');
-  } else if ( Session.get('currentSlide') === 3 ) {
-    this.render('slide3');
-  } else if ( Session.get('currentSlide') === 4 ){
-    this.render('slide4');
-  } else {
-    this.render('slide1');
-  }
+  // if ( Session.get('currentSlide') === 1 ) {
+  //   this.render('slide1');
+  // } else if ( Session.get('currentSlide') === 2 ) {
+  //   this.render('slide2');
+  // } else if ( Session.get('currentSlide') === 3 ) {
+  //   this.render('slide3');
+  // } else if ( Session.get('currentSlide') === 4 ){
+  //   this.render('slide4');
+  // } else if ( Session.get('currentSlide') === 5 ){
+  //   this.render('slide5');
+  // } else {
+  //   this.render('slide1');
+  // }
 });
--- voilah.js
+++ voilah.js
@@ -16,38 +16,63 @@ if (Meteor.isClient) {
   };
 
 
-  var slideInterval = 5000;
-  Template.slide1.rendered = function () {
-    $('#mainChrome').addClass('noUnderBorder');
-    setTimeout(function () {
-      Session.set('currentSlide', 2);
-    }, slideInterval);
-  };
+//   var slideInterval = 5000;
+//   Template.slide1.rendered = function () {
+//     // $(document.body).removeClass('lightBackground');
+//     // $('#mainChrome').removeClass('darkUnderBorder');
+//     // $('#mainChrome').removeClass('lightUpperBorder');
+//     setTimeout(function () {
+//       Session.set('currentSlide', 2);
+//     }, slideInterval);
+//   };
 
-  Template.slide2.rendered = function () {
-    $(document.body).addClass('customBackground');
-    setTimeout(function () {
-      Session.set('currentSlide', 3);
-    }, slideInterval);
-  };
+//   Template.slide2.rendered = function () {
+//     // need this for delay
+//     // $('#mainChrome').removeClass('noUnderBorder');
+//     // $('#mainChrome').removeClass('noUnderBorder');
+//     // $('#mainChrome').removeClass('noUnderBorder');
+//     // // actual css
+//     // $('#mainChrome').addClass('noUnderBorder');
+//     // $(document.body).addClass('lightBackground');
+//     setTimeout(function () {
+//       Session.set('currentSlide', 3);
+//     }, slideInterval);
+//   };
 
-  Template.slide3.rendered = function () {
-    $(document.body).removeClass('customBackground');
-    $(document.body).addClass('noBackground');
-    setTimeout(function () {
-      Session.set('currentSlide', 4);
-    }, slideInterval);
-  };
+//   Template.slide3.rendered = function () {
+// //     $(document.body).removeClass('lightBackground');
+// //     $('#mainChrome').removeClass('noUnderBorder');
+// //     $('#mainChrome').removeClass('noUnderBorder');
+// // // need bigger delay
+// //     $(document.body).addClass('customBackground');
+//     setTimeout(function () {
+//       Session.set('currentSlide', 4);
+//     }, slideInterval);
+//   };
 
-  Template.slide4.rendered = function () {
-    $('#mainChrome').removeClass('noUnderBorder');
-    $(document.body).removeClass('noBackground');
-    $('#mainChrome').addClass('darkUnderBorder');
-    $('#mainChrome').addClass('lightUpperBorder');
-    setTimeout(function () {
-      //Session.set('currentSlide', 1);
-    }, slideInterval);
-  };
+//   Template.slide4.rendered = function () {
+//     // $(document.body).removeClass('customBackground');
+//     // $('#mainChrome').removeClass('noUnderBorder');
+//     // $('#mainChrome').removeClass('noUnderBorder');
+
+//     // $(document.body).addClass('noBackground');
+//     setTimeout(function () {
+//       Session.set('currentSlide', 5);
+//     }, slideInterval);
+//   };
+
+//   Template.slide5.rendered = function () {
+//     // $(document.body).removeClass('noBackground');
+//     // $('#mainChrome').removeClass('noUnderBorder');
+//     // $('#mainChrome').removeClass('noUnderBorder');
+
+//     // $(document.body).addClass('lightBackground');
+//     // $('#mainChrome').addClass('darkUnderBorder');
+//     // $('#mainChrome').addClass('lightUpperBorder');
+//     setTimeout(function () {
+//       //Session.set('currentSlide', 1);
+//     }, slideInterval);
+//   };
 }
 
 if (Meteor.isServer) {
