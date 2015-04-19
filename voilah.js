e9bf53f1604aa2cccbc6d6a59150a6fa58594d9a
William Li <williamli@19degrees.io>
2015-04-19 15:07:03 +0800

    no www

--- voilah.js
+++ voilah.js
@@ -1,5 +1,47 @@
 if (Meteor.isClient) {
   Meteor.startup(function () {
+
+    // no-www
+    WebApp.connectHandlers.use(function(req, res, next) {
+      var is_https = req.headers['x-forwarded-proto'] === "https";
+
+      console.log(moment().format());
+      console.log('encrypted:', is_https);
+      //console.log('headers:', req.headers);
+      console.log(req.headers.host);
+      console.log(req.url);
+      // console.log(req);
+      // console.log(res);
+
+      /* Check if request is for non-www address */
+      var host = req.headers.host.split('.');
+
+      if(host[0].toLowerCase() == 'www') {
+        
+        var protocole = 'http://';
+        if (is_https)
+          protocole = 'https://';
+
+        correctURL = protocole+host.splice(1,2).join('.')+req.url;
+
+        console.log('->',correctURL);
+        res.writeHead(301, {
+          'Content-Type': 'text/html; charset=UTF-8',
+          'Location': correctURL
+        });
+        res.end("Moved to: " + correctURL);
+        return;
+      }
+
+
+
+      next();
+
+    });
+
+
+
+    
     Session.set('curtainLoaded', false);
   });
 
